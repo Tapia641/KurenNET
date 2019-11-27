73b9193f68daf04c9de5e319e4231f4bac13a085
tapia <tapia@localhost.localdomain>
2019-11-27 14:00:05 -0600

    function getPID()

--- prueba.py
+++ prueba.py
@@ -11,6 +11,39 @@ from threading import Timer
 class Kuren:
     interfaz = ""
 
+    def getPID(self, name, upload, download):
+        #GUARDAMOS TODOS LOS PROCESOS ACTIVOS
+        proceso = subprocess.Popen("top > process.txt", shell=True)
+        timer = Timer(10, proceso.kill)
+
+        try:
+            timer.start()
+            stdout, stderr = proceso.communicate()
+            if stdout == None and stderr == None:
+                print("Termina el comando top")
+            else:
+                print(stdout)
+                print(stderr)
+        finally:
+            timer.cancel()
+
+        #DAMOS LECTURA
+        f = open ('process.txt','r')
+        cont = 0
+        pids = []
+        for i in f.readlines():
+            if not cont == 0:
+                if name in i:
+                    string =  i.split(" ")
+                    if not int(string[2]) in pids:
+                        pids.append(int(string[2]))
+            cont = cont + 1
+        f.close()
+        
+        #IMPRIMIMOS LOS PIDS ENCONTRADOS
+        print(pids)
+
+
     def function(self):
         print("---------------------------------------------------")
         os.system("ifconfig")
@@ -30,7 +63,7 @@ class Kuren:
             timer.start()
             stdout, stderr = proc.communicate()
             if stdout == None and stderr == None:
-                print("Correct")
+                print("Termina el comando nethogs")
             else:
                 print(stdout)
                 print(stderr)
@@ -40,3 +73,4 @@ class Kuren:
 n = Kuren()
 n.function()
 n.outputData()
+n.getPID("firefox", "", "")
