3e5e3c329b5ad710d64b262f7b87775189f5c312
tapia <tapia@localhost.localdomain>
2019-11-28 11:48:43 -0600

    Function killProcess added

--- prueba.py
+++ prueba.py
@@ -11,6 +11,8 @@ from threading import Timer
 
 class Kuren:
     interfaz = ""
+    important = 0
+    limite = 0
 
     def dataInput(self):
         print("---------------------------------------------------")
@@ -19,6 +21,15 @@ class Kuren:
         print("---------------------------------------------------")
         print("Ingresa el nombre de la interfáz a monitorear:")
         self.interfaz = str(input())
+        os.system("clear")
+        os.system("ps aux")
+        print("---------------------------------------------------")
+        print("Ingresa el numero del proceso a no interferir:")
+        self.important = int(input())
+        os.system("clear")
+        print("---------------------------------------------------")
+        print("Ingresa el limite en kbs:")
+        self.limite = float(input())
         os.system("clear")
 
     def saveOutput(self):
@@ -41,7 +52,7 @@ class Kuren:
     def getName(self):
         f = open ('OUTPUT.txt','r+')
         lines = f.readlines()
-        A =dict()
+        A = dict()
         for i in range(len(lines)):
             if lines[i] == "Refreshing:\n":
                 l = lines[i+1]                      
@@ -53,18 +64,19 @@ class Kuren:
                     # print("THIS IS : {} {} {}".format(g[1],g[2],x[2]))                
                 #      A.append(x[2])
         f.close()
+        print("Esto es A:", A)
         return A       
         
-    def getPID(self, name):
+    def getPID(self, myDic):
         #GUARDAMOS TODOS LOS PROCESOS ACTIVOS
         #EL COMANDO TOP HACE QUE LA TERMINAL SE TRABE
-        proceso = subprocess.Popen("top > PROCESS.txt", shell=True)
+        proceso = subprocess.Popen("ps aux > PROCESS.txt", shell=True)
         timer = Timer(10, proceso.kill)
         try:
             timer.start()
             stdout, stderr = proceso.communicate()
             if stdout == None and stderr == None:
-                print("Termina el comando top")
+                print("Termina el comando ps aux")
             else:
                 print(stdout)
                 print(stderr)
@@ -78,8 +90,10 @@ class Kuren:
 
         for i in f.readlines():
             if not cont == 0:
-                for n in name:
-                    if n in i:
+                for n in myDic:
+                    values = myDic.get(n)
+                    #print("Comparando value[0]: ", float(values[0]), " >= ", self.limite)
+                    if n in i and float(values[0]) >= self.limite:
                         # #SE REMUEVE CARACTERES DESCONOCIDOS                        
                         string =  i.replace("[^\\dA-Za-z]", "")
                         string =  string.replace("\x1b(B\x1b[m", "")                        
@@ -94,20 +108,29 @@ class Kuren:
                                 lista.append(j)
                         
                         # #PREGUNTAMOS SI EL PID NO ESTA EN LA LISTA
-                        if not lista[0] in PIDS:
-                            PIDS.append(lista[0])
+                        if not lista[1] in PIDS:
+                            PIDS.append(lista[1])
             cont = cont + 1
         f.close()
         
-        #IMPRIMIMOS LOS PIDS ENCONTRADOS
-        print(PIDS)
+        #RETORNAMOS LOS PIDS ENCONTRADOS
+        return PIDS
 
-
+    def killProcess(self, LISTA):
+        for i in LISTA:
+            print("i: ", i)
+            if not int(i) == self.important:
+                os.system("kill -TERM %i" % int(i))
+                print("Se mato al proceso: ", i)
+                self.sendNotificacion()
+    
+    def sendNotificacion(self):
+        pass
 
 if __name__ == "__main__":
     n = Kuren()
     n.dataInput()
     n.saveOutput()
     A = n.getName()
-    print(A)
-    # n.getPID(A)
+    L = n.getPID(A)
+    n.killProcess(L)
